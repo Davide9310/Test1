347860b515b12884dba6b89d89b4b48f2fce7400
Davide9310 <57132551+Davide9310@users.noreply.github.com>
2024-08-29 21:41:58 -0400

    Add files via upload

---
+++ test1.js
@@ -0,0 +1,21 @@
+import Replicate from "replicate";
+
+const replicate = new Replicate({
+  auth: process.env.REPLICATE_API_TOKEN,
+});
+
+const input = {
+  top_k: 50,
+  top_p: 0.9,
+  prompt: "Tina has one brother and one sister. How many sisters do Tina's siblings have?",
+  max_tokens: 1024,
+  min_tokens: 0,
+  temperature: 0.6,
+  system_prompt: "You are a helpful assistant.",
+  presence_penalty: 0,
+  frequency_penalty: 0
+};
+
+for await (const event of replicate.stream("meta/meta-llama-3.1-405b-instruct", { input })) {
+  process.stdout.write(event.toString());
+};
